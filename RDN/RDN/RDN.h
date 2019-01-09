546d796e07cd03c5479946e4d9f26339d70c3404
David Wu <wei.b.wu@rakuten.com>
2019-01-09 11:14:07 +0900

    generate by xcode 10.1

---
+++ RDN/RDN/RDN.h
@@ -0,0 +1,19 @@
+//
+//  RDN.h
+//  RDN
+//
+//  Created by Wu, Wei b on 2019/01/09.
+//  Copyright © 2019 Rakuten MPD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RDN.
+FOUNDATION_EXPORT double RDNVersionNumber;
+
+//! Project version string for RDN.
+FOUNDATION_EXPORT const unsigned char RDNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDN/PublicHeader.h>
+
+
