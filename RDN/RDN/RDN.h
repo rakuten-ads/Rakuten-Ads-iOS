b2ad33ac2dbe48a248b6ec8523dddebec0dfc780
David Wu <wei.b.wu@rakuten.com>
2019-01-17 17:26:04 +0900

    build scripts & RDN module settings

--- RDN/RDN/RDN.h
+++ RDN/RDN/RDN.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char RDNVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RDN/PublicHeader.h>
 
-
+#import "GAPAdView.h"
