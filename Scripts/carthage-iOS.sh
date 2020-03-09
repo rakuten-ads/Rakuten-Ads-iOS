ba6372c3a6b7e6d34a18b6ad5015b1eba820e048
wei.wu <wei.b.wu@rakuten.com>
2020-03-09 18:15:03 +0900

    share aggregate script

--- Scripts/carthage-iOS.sh
+++ Scripts/carthage-iOS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #  carthage-iOS.sh
-#  RUNACore
+#  RUNA
 #
 #  Created by Wu Wei on 2019/01/15.
 #  Copyright © 2018 LOB. All rights reserved.
@@ -10,7 +10,12 @@ echo
 echo "===[$0] START==="
 set -eo pipefail
 
-SDK_PRODUCT_NAME=RUNACore
+SDK_PRODUCT_NAME=$1
+if [ ! $SDK_PRODUCT_NAME ]; then
+	echo "must give a product name"
+	exit 1
+fi
+
 if [ ! $SDK_OUTPUT_DIR ]; then
 	SDK_OUTPUT_DIR=${PROJECT_DIR}/build/${CONFIGURATION}
 fi
