4b9a28c2e6455116bb64d96dafa83f13c5c43186
Wu, Wei | David <wei.b.wu@rakuten.com>
2020-08-21 15:51:28 +0900

    OMAdapter modulemap

--- OMAdapter/OMAdapter/RUNAOMAdapter.h
+++ OMAdapter/OMAdapter/RUNAOMAdapter.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char OMAdapterVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <OMAdapter/PublicHeader.h>
 
 
+#import "RUNABannerViewOM.h"
