7cc81d1594399d485d4deef3036807c53bb0b1ae
Wu, Wei | David <wei.b.wu@rakuten.com>
2021-06-02 10:54:55 +0900

    chore blank

--- Banner/RUNA/Measurement/RUNAMeasurement.h
+++ Banner/RUNA/Measurement/RUNAMeasurement.h
@@ -30,7 +30,6 @@ NS_ASSUME_NONNULL_BEGIN
  @param isInview result of in view measured
  @return YES for completing measurement, otherwise false
  */
-
 - (BOOL)didMeasureInview:(BOOL)isInview;
 
 @end
