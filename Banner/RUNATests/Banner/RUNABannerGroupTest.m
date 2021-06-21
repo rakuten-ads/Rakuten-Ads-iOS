9a899fc31e74967292bd59a693e8296a8477ef9b
Akihiko Sato <akihiko.sato@rakuten.com>
2021-06-21 17:51:10 +0900

    add testSetBanners

--- Banner/RUNATests/Banner/RUNABannerGroupTest.m
+++ Banner/RUNATests/Banner/RUNABannerGroupTest.m
@@ -12,6 +12,7 @@
 
 @interface RUNABannerGroup (Spy)
 @property (nonatomic, readonly) RUNABannerViewState state;
+@property (nonatomic, readonly) NSDictionary<NSString*, RUNABannerView*> *bannerDict;
 - (instancetype)init;
 @end
 
@@ -24,6 +25,15 @@
 - (void)testInit {
     RUNABannerGroup *group = [[RUNABannerGroup alloc]init];
     XCTAssertEqual(group.state, RUNA_ADVIEW_STATE_INIT);
+}
+
+- (void)testSetBanners {
+    RUNABannerGroup *group = [[RUNABannerGroup alloc]init];
+    [group setBanners:@[[RUNABannerView new]]];
+    NSDictionary *actual = group.bannerDict;
+    XCTAssertNotNil(actual);
+    XCTAssertEqual(actual.allKeys.count, (NSUInteger)1);
+    XCTAssertNotNil(actual[group.bannerDict.allKeys[0]]);
 }
 
 @end
