d09a8ad4669431c931cf806482283c7c549a5310
Akihiko Sato <akihiko.sato@rakuten.com>
2021-06-22 09:45:21 +0900

    add testBanners

--- Banner/RUNATests/Banner/RUNABannerGroupTest.m
+++ Banner/RUNATests/Banner/RUNABannerGroupTest.m
@@ -14,6 +14,7 @@
 @property (nonatomic, readonly) RUNABannerViewState state;
 @property (nonatomic, readonly) NSDictionary<NSString*, RUNABannerView*> *bannerDict;
 - (instancetype)init;
+- (NSArray<RUNABannerView*>*)banners;
 @end
 
 @interface RUNABannerGroupTest : XCTestCase
@@ -28,12 +29,32 @@
 }
 
 - (void)testSetBanners {
+    // Case: one object
+    {
+        RUNABannerGroup *group = [[RUNABannerGroup alloc]init];
+        [group setBanners:@[[RUNABannerView new]]];
+        NSDictionary *actual = group.bannerDict;
+        XCTAssertNotNil(actual);
+        XCTAssertEqual(actual.allKeys.count, (NSUInteger)1);
+        XCTAssertNotNil(actual[group.bannerDict.allKeys[0]]);
+    }
+    // Case: Two objects
+    {
+        RUNABannerGroup *group = [[RUNABannerGroup alloc]init];
+        [group setBanners:@[[RUNABannerView new], [RUNABannerView new]]];
+        NSDictionary *actual = group.bannerDict;
+        XCTAssertNotNil(actual);
+        XCTAssertEqual(actual.allKeys.count, (NSUInteger)2);
+        XCTAssertNotNil(actual[group.bannerDict.allKeys[0]]);
+        XCTAssertNotNil(actual[group.bannerDict.allKeys[1]]);
+    }
+}
+
+- (void)testBanners {
     RUNABannerGroup *group = [[RUNABannerGroup alloc]init];
-    [group setBanners:@[[RUNABannerView new]]];
-    NSDictionary *actual = group.bannerDict;
-    XCTAssertNotNil(actual);
-    XCTAssertEqual(actual.allKeys.count, (NSUInteger)1);
-    XCTAssertNotNil(actual[group.bannerDict.allKeys[0]]);
+    [group setBanners:@[[RUNABannerView new], [RUNABannerView new]]];
+    NSArray *actuals = [group banners];
+    XCTAssertEqual(actuals.count, (NSUInteger)2);
 }
 
 @end
