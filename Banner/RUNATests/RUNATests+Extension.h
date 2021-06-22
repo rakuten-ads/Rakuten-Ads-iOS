0187402063594bd947a37af7809f5f02a3910be9
Akihiko Sato <akihiko.sato@rakuten.com>
2021-06-22 11:23:36 +0900

    add Extension

---
+++ Banner/RUNATests/RUNATests+Extension.h
@@ -0,0 +1,17 @@
+//
+//  RUNATests+Extension.h
+//  BannerTests
+//
+//  Created by Sato, Akihiko | Akkie on 2021/06/22.
+//  Copyright © 2021 Rakuten MPD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTest (Extension)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Banner/RUNATests/RUNATests+Extension.m
@@ -0,0 +1,13 @@
+//
+//  RUNATests+Extension.m
+//  BannerTests
+//
+//  Created by Sato, Akihiko | Akkie on 2021/06/22.
+//  Copyright © 2021 Rakuten MPD. All rights reserved.
+//
+
+#import "RUNATests+Extension.h"
+
+@implementation XCTest (Extension)
+
+@end
