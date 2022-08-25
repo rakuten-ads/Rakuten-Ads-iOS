2d85349fd26310504feff8745ecbbc62e482cb0d
wei.b.wu-pro14 <wei.b.wu@rakuten.com>
2022-08-25 19:56:30 +0900

    carthage build xcframework

--- release/carthage-iOS.sh
+++ release/carthage-iOS.sh
@@ -10,6 +10,8 @@ echo
 echo "===[$0] START==="
 set -eo pipefail
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 SDK_PRODUCT_NAME=$1
 if [ ! $SDK_PRODUCT_NAME ]; then
 	echo "must give a product name"
@@ -30,35 +32,30 @@ if [ "$1" != "archive" ]; then
 	fi
 
 	echo "carthage build for ${SDK_PRODUCT_NAME} $SDK_VERSION_STRING in configuration $CONFIGURATION"
-	carthage build --configuration $CONFIGURATION --platform iOS --no-skip-current --project-directory ${PROJECT_DIR}
+	carthage build --use-xcframeworks --configuration $CONFIGURATION --platform iOS --no-skip-current --project-directory ${PROJECT_DIR}
 
 fi
 
-SDK_NAME=${SDK_PRODUCT_NAME}_iOS_${SDK_VERSION_STRING}.framework.zip
-SDK_DSYM_NAME=${SDK_PRODUCT_NAME}_iOS_${SDK_VERSION_STRING}.dsym.zip
-SDK_OUTPUT=${SDK_OUTPUT_DIR}/$SDK_NAME
-rm -f $SDK_OUTPUT
-echo "carthage archive to $SDK_OUTPUT for ${SDK_PRODUCT_NAME}"
-carthage archive --output $SDK_OUTPUT ${SDK_PRODUCT_NAME}
-if test -f $SDK_OUTPUT; then
-	echo "-----prune dSYM files -----"
-	cd $SDK_OUTPUT_DIR
-	rm -fr ./Carthage
-	unzip -o $SDK_NAME -d .
+SDK_BUILD_FILE=$SDK_PRODUCT_NAME.xcframework
+SDK_OUTPUT_FILE=${SDK_OUTPUT_DIR}/${SDK_PRODUCT_NAME}_iOS_${SDK_VERSION_STRING}.xcframework.zip
+SDK_OUTPUT_FILE_DSYM=${SDK_OUTPUT_DIR}/${SDK_PRODUCT_NAME}_iOS_${SDK_VERSION_STRING}+dsym.zip
 
-	SDK_DSYM_PATH=DSYM_$SDK_VERSION_STRING
-	rm -fr $SDK_DSYM_PATH
-	mkdir $SDK_DSYM_PATH
-	find ./Carthage -name *.dSYM -o -name *.bcsymbolmap | xargs -I{} mv {} $SDK_DSYM_PATH/
-	rm -f $SDK_DSYM_NAME
-	zip -r $SDK_DSYM_NAME $SDK_DSYM_PATH -x .DS_Store
-
-	rm $SDK_NAME
-
-	# find . -name *.dSYM -o -name *.bcsymbolmap | xargs rm -r
-	zip -r $SDK_NAME ./Carthage -x .DS_Store
-	open .
+if [ ! -d $SDK_OUTPUT_DIR ]; then
+	mkdir $SDK_OUTPUT_DIR
 fi
 
-cd $PROJECT_DIR
+cd $SDK_BUILD_DIR
+if [ -d $SDK_BUILD_FILE ]; then
+	# zip sdk+dsym
+	zip -r $SDK_OUTPUT_FILE_DSYM $SDK_BUILD_FILE -x ".DS_Store"
+
+	# zip sdk
+	find . -name "*.dSYM" -exec rm -r -- {} +
+	find . -name "*.bcsymbolmap" -exec rm -- {} +
+	find . -name ".DS_Store" -exec rm -r -- {} +
+	find . -empty -type d -delete
+	zip -r $SDK_OUTPUT_FILE $SDK_BUILD_FILE -x ".DS_Store"
+fi
+open $SDK_OUTPUT_DIR
+
 echo "===[$0] END==="
