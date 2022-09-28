9f413e7bd60a7bb803d51e2c501cc58d654cc9b4
wei.b.wu-pro14 <wei.b.wu@rakuten.com>
2022-09-29 00:53:00 +0900

    binaryTarget name must be as same as xcframework name

--- Package.swift
+++ Package.swift
@@ -10,39 +10,39 @@ let package = Package(
         .library(
             name: "RUNABanner",
             targets: [
-                "Core",
-                "Banner",
+                "RUNACore",
+                "RUNABanner",
             ]
         ),
         .library(
             name: "RUNAOMAdapter",
             targets: [
-                "Core",
-                "Banner",
-                "OMAdapter",
-                "OMSDK",
+                "RUNACore",
+                "RUNABanner",
+                "RUNAOMAdapter",
+                "OMSDK_Rakuten",
             ]
         ),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "Core",
+            name: "RUNACore",
             url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.4.6.xcframework.zip",
             checksum : "a5aae671af5dd526e1ab4cdb4a8bfd411ed38c831b1b67657f90f4cb9000272a"
         ),
         .binaryTarget(
-            name: "Banner",
+            name: "RUNABanner",
             url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.9.2.xcframework.zip",
             checksum : "386b76dec8fd0d49a8ae7704809d88aa049f3859caaf02acd9581f19dec73d1d"
         ),
         .binaryTarget(
-            name: "OMAdapter",
+            name: "RUNAOMAdapter",
             url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.0.8.xcframework.zip",
             checksum : "458affe2462cc833f072f69616f1f2faf8b8139f959d959196429fd83fd9790d"
         ),
         .binaryTarget(
-            name: "OMSDK",
+            name: "OMSDK_Rakuten",
             url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMSDK/RUNAOMSDK_iOS_1.3.31.xcframework.zip",
             checksum : "9f5e075651c1c254f6016dd9bc2e8d8e45d6ba2843f8d4c4299e6de6446b31ed"
         ),
