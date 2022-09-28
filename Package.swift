bc8f44abb296e9a5fa707e14e85b0194b84cb59e
wei.b.wu-pro14 <wei.b.wu@rakuten.com>
2022-09-29 00:05:53 +0900

    update checksum: Core, Banner, OMAdapter

--- Package.swift
+++ Package.swift
@@ -24,21 +24,21 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Core",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.4.5.xcframework.zip",
-            checksum : "14a04c8daf65595004027ae830c09c70a1fc6e63995ae2f3b5b149786ed8eba0"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.4.6.xcframework.zip",
+            checksum : "a5aae671af5dd526e1ab4cdb4a8bfd411ed38c831b1b67657f90f4cb9000272a"
         ),
         .binaryTarget(
             name: "Banner",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.9.1.xcframework.zip",
-            checksum : "db1d699ecd2239e537ba2372fecf98783c2d8a84e83f0b00193a38ae5a793db1",
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.9.2.xcframework.zip",
+            checksum : "386b76dec8fd0d49a8ae7704809d88aa049f3859caaf02acd9581f19dec73d1d",
             dependencies: [
                 "Core"
             ]
         ),
         .binaryTarget(
             name: "OMAdapter",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.0.7.xcframework.zip",
-            checksum : "3e8ef20acf3ed3514e25130a7b689ea5f66eb1d9cdd608e697597dca950c5c3e",
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.0.8.xcframework.zip",
+            checksum : "458affe2462cc833f072f69616f1f2faf8b8139f959d959196429fd83fd9790d",
             dependencies: [
                 "Banner",
                 "OMSDK",
