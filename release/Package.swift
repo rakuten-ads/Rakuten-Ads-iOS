cc1b1ef0da41cefeca010a8ee95c14dea7cc332c
wei-b-wu-rakuten <wei.b.wu@rakuten.com>
2023-10-24 16:01:26 +0900

    compute spm script

---
+++ release/Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RUNA",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "RUNABanner",
+            targets: [
+                "RUNACore",
+                "RUNABanner",
+            ]
+        ),
+        .library(
+            name: "RUNAOMAdapter",
+            targets: [
+                "RUNACore",
+                "RUNABanner",
+                "RUNAOMAdapter",
+                "OMSDK_Rakuten",
+            ]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "RUNACore",
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.7.0.xcframework.zip",
+			checksum : "9785efa3aefd25019330c33029dd62da84804cc05c035640ac39a36319affea0"
+        ),
+        .binaryTarget(
+            name: "RUNABanner",
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.13.0.xcframework.zip",
+            checksum : "dfc031fc6f1929e4b95ceecabed09b90e9c6fb816b00310d98acefe2faf71ca1"
+        ),
+        .binaryTarget(
+            name: "RUNAOMAdapter",
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.2.0.xcframework.zip",
+            checksum : "d80ab79fc780e0a07df7326d3767abf17779caf9d0cb9789b19f6f9e8ddba053"
+        ),
+        .binaryTarget(
+            name: "OMSDK_Rakuten",
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMSDK/RUNAOMSDK_iOS_1.4.3.xcframework.zip",
+            checksum : "aa5a99ae1f1b64bac66b0beffa2aaadc8920842c3706aeb7e0508b19d29d70d5"
+        ),
+    ]
+)
