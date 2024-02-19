572318eeff40d0a93efb7b8dacf18325c0d60e30
wei-b-wu-rakuten <wei.b.wu@rakuten.com>
2024-02-19 14:38:00 +0900

    README of release

--- release/Package.swift
+++ release/Package.swift
@@ -28,18 +28,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RUNACore",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.7.0.xcframework.zip",
-			checksum : "9785efa3aefd25019330c33029dd62da84804cc05c035640ac39a36319affea0"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.8.0.xcframework.zip",
+			checksum : "9a61185eb755bcfe73bec8f869064e86576dd9ecdff3563151eb8496fa15b621"
         ),
         .binaryTarget(
             name: "RUNABanner",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.13.0.xcframework.zip",
-            checksum : "dfc031fc6f1929e4b95ceecabed09b90e9c6fb816b00310d98acefe2faf71ca1"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.14.0.xcframework.zip",
+			checksum : "33cc7ee8507a519e456ff62c48ee36c0cf7af92b1c16ae540fe29dd94de03fe3"
         ),
         .binaryTarget(
             name: "RUNAOMAdapter",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.2.0.xcframework.zip",
-            checksum : "d80ab79fc780e0a07df7326d3767abf17779caf9d0cb9789b19f6f9e8ddba053"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.3.0.xcframework.zip",
+			checksum : "970ad2d481a91d40dc1634bd10bb61c68476398807c158157d3ef0d574f39846"
         ),
         .binaryTarget(
             name: "OMSDK_Rakuten",
