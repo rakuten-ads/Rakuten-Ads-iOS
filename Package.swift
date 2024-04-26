fae06a0be7d48004a51db3749dde7f40c800a57d
wei-b-wu-rakuten <wei.b.wu@rakuten.com>
2024-04-26 10:55:41 +0900

    update SPM

--- Package.swift
+++ Package.swift
@@ -28,23 +28,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RUNACore",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.8.1.xcframework.zip",
-			checksum : "db07ab7e04b7f33426f59130fcc4a74996681ee27907f1991e428f46608ddc7a"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNACore/RUNACore_iOS_1.8.2.xcframework.zip",
+			checksum : "305fed8674362ffdda2e29403f45aaf6fab0a402d4dc0686cd25417ede4ebb58"
         ),
         .binaryTarget(
             name: "RUNABanner",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.14.1.xcframework.zip",
-			checksum : "ca933f86b355b5ab401f693facfae4b76735825d83f3682abc8f418d3930bf0b"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNABanner/RUNABanner_iOS_1.14.2.xcframework.zip",
+			checksum : "8a2046dd3ab255ef63a983dedef4699ee7db3f03cfe95b97b15784595f33d89c"
         ),
         .binaryTarget(
             name: "RUNAOMAdapter",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.3.1.xcframework.zip",
-			checksum : "fb56046598777bd19a1b8a3c6a9dfd5b46df54d0bccb352ca4e753ff55d7d644"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMAdapter/RUNAOMAdapter_iOS_1.3.2.xcframework.zip",
+			checksum : "221e2e02b8db0e84b1af5e8a6c9b3c403e858a81d4b69bedacc520c421d880d5"
         ),
         .binaryTarget(
             name: "OMSDK_Rakuten",
-            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMSDK/RUNAOMSDK_iOS_1.4.12.xcframework.zip",
-            checksum : "07e994bfa8f236979d61a2b5c4bff2a2ecaf011a394aaee4c6f71f9f631907c9"
+            url: "https://storage.googleapis.com/rssp-dev-cdn/sdk/ios/prod/RUNAOMSDK/RUNAOMSDK_iOS_1.4.13.xcframework.zip",
+            checksum : "180907f36fd797969839123a4845a7e929af7ee7f2f51326419e3060fef551e0"
         ),
     ]
 )
